0c83c78a1d93767f053195a5ba0fe43b9367c1bb
tjleizeng <tjleizeng@users.noreply.github.com>
2022-10-21 09:51:36 -0400

    File cleaning.

--- ADDSEVS/test/commands_sim.sh
+++
@@ -1,9 +0,0 @@
-/usr/lib/jvm/jdk8u202-b08/bin/java \
--Xss256M -Xms1024M -Xmx4G -Xshare:off -Dfile.encoding=UTF-8 -classpath \
-/home/charitha/git/doe-mets-r/EvacSim/lib/servlet-api-3.1.jar:\
-/home/charitha/.p2/pool/plugins/org.codehaus.groovy_2.4.16.xx-201812291959-e2018-12-RELEASE/lib/*:\
-/home/charitha/eclipse/plugins/repast.simphony.runtime_2.6.0/bin:\
-/home/charitha/eclipse/plugins/repast.simphony.runtime_2.6.0/lib/*:\
-/home/charitha/git/doe-mets-r/EvacSim \
-repast.simphony.runtime.RepastMain \
-/home/charitha/git/doe-mets-r/EvacSim/EvacSim.rs
